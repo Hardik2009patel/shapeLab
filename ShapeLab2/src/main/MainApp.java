9379925acf464d8a7ccbd5cc998b7b9ee56655d8
Paul Turnbaugh <pet30200@gmail.com>
2016-02-24 21:20:21 -0500

    Part of the Main app and file

---
+++ ShapeLab2/src/main/MainApp.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+import shapes.*;
+
+public class MainApp {
+
+	public static void main(String[] args) throws IOException {
+		File dimentions = new File("ShapeDimentions.csv");
+		Scanner file = new Scanner(dimentions);
+		
+		
+		ArrayList<file> numbers = new ArrayList();
+		
+
+	}
+
+}
